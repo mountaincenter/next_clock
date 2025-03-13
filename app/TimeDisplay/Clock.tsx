9f53ed5ae4cb90b30010981b0b539596fcefdc0e
mountaincenter <y.hiroyuki560730@gmail.com>
2025-03-13 12:07:46 +0900

    add styling tabular-nums

--- app/TimeDisplay/Clock.tsx
+++ app/TimeDisplay/Clock.tsx
@@ -5,6 +5,7 @@ import { format } from "date-fns";
 
 const Clock = () => {
   const [time, setTime] = useState("");
+
   useEffect(() => {
     const interval = setInterval(() => {
       const now = new Date();
@@ -14,7 +15,14 @@ const Clock = () => {
     }, 1000);
     return () => clearInterval(interval);
   }, []);
-  return <div className="text-[14rem]">{time}</div>;
+
+  return (
+    <div className="flex justify-center">
+      <span className="text-[12rem] min-w-[32rem] text-center font-sans tabular-nums">
+        {time}
+      </span>
+    </div>
+  );
 };
 
 export default Clock;
